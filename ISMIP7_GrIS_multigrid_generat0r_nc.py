53349e59a5daff75f236ea5595cb09c2eac0b1c0
hgoelzer <heig@norceresearch.no>
2024-05-24 17:47:31 +0200

    initial checkin of scripts

---
+++ ISMIP7_GrIS_multigrid_generat0r_nc.py
@@ -0,0 +1,98 @@
+# Write ISMIP7 grid files in CDO format
+
+import numpy as np
+from generate_CDO_files_nc import generate_CDO_files
+
+# for checking
+def isaninteger(x):
+    return np.mod(x, 1) == 0
+
+##### Typically the only part a user needs to modify
+# Specify various ISM grids at different resolution
+rk = [16]
+#rk = [16, 8, 4, 2, 1]
+#rk = [0.5]
+
+# Choose which output file to write
+flag_nc = True
+flag_xy = True
+flag_af2 = True
+#####
+
+# Output angle type (degrees or radians)
+output_data_type = 'degrees'
+
+# Write additional g0 grid files
+flag_g0 = False
+
+# Mapping information. This is EPSG 3413 for GrIS
+proj_info = {}
+proj_info['earthradius'] = 6378137.0
+proj_info['eccentricity'] = 0.081819190842621
+proj_info['standard_parallel'] = 70.
+proj_info['longitude_rot'] = 315.
+proj_info['hemisphere'] = 'north'
+# Offset of grid node centers. Lower left corner coordinates.
+# Note sign change compared to matlab version!
+proj_info['falseeasting'] = -720000
+proj_info['falsenorthing'] = -3450000
+
+# Grid dimensions of 1 km base grid
+nx_base = 1681
+ny_base = 2881
+
+
+# g1 grid where ice thickness and SMB are defined
+grids1 = []
+for r in rk:
+    # For any resolution but check integer grid numbers
+    nx = ((nx_base-1)/r)+1
+    ny = ((ny_base-1)/r)+1
+    if isaninteger(nx) and isaninteger(ny):
+        agrid = {}
+        agrid['dx'] = r*1000.
+        agrid['dy'] = r*1000.
+        agrid['nx'] = int(nx)
+        agrid['ny'] = int(ny)
+        agrid['offsetx'] = 0.
+        agrid['offsety'] = 0.
+        agrid['LatLonOutputFileName'] = 'grid_ISMIP7_g1_GrIS_{:05d}m.nc'.format(int(r*1000))
+        agrid['xyOutputFileName'] = 'xy_ISMIP7_g1_GrIS_{:05d}m.nc'.format(int(r*1000))
+        agrid['af2OutputFileName'] = 'af2_ISMIP7_g1_GrIS_{:05d}m.nc'.format(int(r*1000))
+        grids1.append(agrid)
+    else:
+        print('Warning: resolution {} km is not comensurable, skipped.'.format(r))
+
+# Create grids and write out
+for agrid in grids1:
+    #print(agrid)
+    success = generate_CDO_files(agrid, proj_info, output_data_type, flag_nc, flag_xy, flag_af2)
+
+
+if flag_g0:
+    # g0 grid where horizontal velocities are defined e.g. for CISM 
+    grids0 = []
+    for r in rk:
+        # For any resolution but check integer grid numbers
+        nx = ((nx_base-1)/r)
+        ny = ((ny_base-1)/r)
+        if isaninteger(nx) and isaninteger(ny):
+            agrid = {}
+            agrid['dx'] = r*1000.
+            agrid['dy'] = r*1000.
+            agrid['nx'] = int(nx)
+            agrid['ny'] = int(ny)
+            # g0 grid is offset by half a grid size
+            agrid['offsetx'] = r*1000./2.
+            agrid['offsety'] = r*1000./2.
+            agrid['LatLonOutputFileName'] = 'grid_ISMIP7_g0_GrIS_{:05d}m.nc'.format(int(r*1000))
+            agrid['xyOutputFileName'] = 'xy_ISMIP7_g0_GrIS_{:05d}m.nc'.format(int(r*1000))
+            agrid['af2OutputFileName'] = 'af2_ISMIP7_g0_GrIS_{:05d}m.nc'.format(int(r*1000))
+            grids0.append(agrid)
+        else:
+            print('Warning: resolution {} km is not comensurable, skipped.'.format(r))
+            
+            # Create grids and write out
+            for agrid in grids0:
+                #print(agrid)
+                success = generate_CDO_files(agrid, proj_info, output_data_type, flag_nc, flag_xy, flag_af2)
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # generate-ismip7-grid-files-py
 Python scripts to generate ISMIP7 grid description files used for CDO regridding.
+
+## Setup
+Needs a python3 environment with  
+numpy, netCDF4, os
+
+The scripts can generate 3 different types of files for Greenland and Antarctica
+
+### grid description files (needed for CDO regridding)
+  grid_ISMIP7_g?_IS_res.nc
+### xy coordinates 
+  xy_ISMIP7_g?_IS_res.nc
+### area factors 
+  af2_ISMIP7_g?_IS_res.nc
+
+All files are produced for the diagnostic grid g1 (ice thickness, SMB, ..) 
+The scripts can also generate files for a staggered grid g0, where e.g. CISM defines horizontal velocities. 
+  ISMIP7_AIS_multigrid_generator_nc.py  
+  ISMIP7_GrIS_multigrid_generator_nc.py  
+
+using  
+
+  polar_stereo.py  
+  wnc.py  
